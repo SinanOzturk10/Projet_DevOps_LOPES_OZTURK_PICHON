213b4799d481eddc89e9523e2d337a5d5be2403b
Lopes <allan.lopes@etu.univ-grenoble-alpes.fr>
2026-04-06 15:42:56 +0200

    Factory pour 1D fait

--- LopesOzturkPichon/src/main/java/ProjetDevOpz/factory/NdArrayFactory.java
+++ LopesOzturkPichon/src/main/java/ProjetDevOpz/factory/NdArrayFactory.java
@@ -7,11 +7,42 @@ import ProjetDevOpz.core.Matrix2D;
 public class NdArrayFactory {
 
     // public api (5.1 subject)
-    public static Vector1D array(float[] data)              { throw new UnsupportedOperationException(); }
+    public static Vector1D array(float[] data) {
+        return new Vector1D(data);
+    }
+
+
     public static Matrix2D  array(float[][] data)           { throw new UnsupportedOperationException(); }
-    public static NdArray   zeros(int... shape)             { throw new UnsupportedOperationException(); }
-    public static NdArray   arange(float start, float stop) { throw new UnsupportedOperationException(); }
+
+
+    public static NdArray zeros(int... shape) {
+        if (shape.length != 1) {
+            throw new IllegalArgumentException("Only 1D zeros is supported for now");
+        }
+
+        return new Vector1D(new float[shape[0]]);
+    }
+
+    public static NdArray arange(float start, float stop) {
+        int length = (int) (stop - start);
+        if (length < 0) {
+            throw new IllegalArgumentException("stop must be greater than or equal to start");
+        }
+
+        float[] res = new float[length];
+        for (int i = 0; i < length; i++) {
+            res[i] = start + i;
+        }
+
+        return new Vector1D(res);
+    }
 
     // for the reshape() method
-    public static NdArray fromRaw(float[] data, int[] shape)       { throw new UnsupportedOperationException(); }
+    public static NdArray fromRaw(float[] data, int[] shape) {
+        if (shape.length == 1) {
+            return new Vector1D(data);
+        }
+
+        throw new IllegalArgumentException("Only 1D zeros is supported for now");
+    }
 }
